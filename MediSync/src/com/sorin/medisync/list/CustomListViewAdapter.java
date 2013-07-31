a348ca92947fb6ad19470351be1df41ae5b22e92
irimies <irimies@NB-2012-01-02-0.s1nn.int>
2013-07-31 17:15:10 +0200

    Added Cursor Adapter

---
+++ MediSync/src/com/sorin/medisync/list/CustomListViewAdapter.java
@@ -0,0 +1,55 @@
+package com.sorin.medisync.list;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.sorin.medisync.R;
+
+public class CustomListViewAdapter extends ArrayAdapter<ListViewStoreData> {
+
+	Context context;
+
+	public CustomListViewAdapter(Context context, int resourceId,
+			List<ListViewStoreData> items) {
+		super(context, resourceId, items);
+		this.context = context;
+	}
+
+	/* private view holder class */
+	private class ViewHolder {
+		ImageView imageView;
+		TextView txtTitle;
+		TextView txtDesc;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder = null;
+		ListViewStoreData rowItem = getItem(position);
+
+		LayoutInflater mInflater = (LayoutInflater) context
+				.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+		if (convertView == null) {
+			convertView = mInflater.inflate(R.layout.patient_item, null);
+			holder = new ViewHolder();
+			holder.txtDesc = (TextView) convertView.findViewById(R.id.desc);
+			holder.txtTitle = (TextView) convertView.findViewById(R.id.title);
+			holder.imageView = (ImageView) convertView.findViewById(R.id.icon);
+			convertView.setTag(holder);
+		} else
+			holder = (ViewHolder) convertView.getTag();
+
+		holder.txtDesc.setText(rowItem.getDesc());
+		holder.txtTitle.setText(rowItem.getTitle());
+		holder.imageView.setImageResource(rowItem.getImageId());
+
+		return convertView;
+	}
+}
