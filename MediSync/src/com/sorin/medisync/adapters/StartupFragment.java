4313036a4457a67368e4474b0a02a7cea295f240
greenspand <mihaiirimies@gmail.com>
2013-08-01 11:11:02 +0200

    added animations

---
+++ MediSync/src/com/sorin/medisync/adapters/StartupFragment.java
@@ -0,0 +1,17 @@
+package com.sorin.medisync.adapters;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sorin.medisync.R;
+
+public class StartupFragment extends Fragment {
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.startup, null);
+	}
+}
